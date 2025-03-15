9e3077e9caec4daa246e2bee0a6a5aeebd6e70e5
Mihai Prodan <florin_mihai.prodan@stud.fils.upb.ro>
2025-03-15 16:12:37 +0200

    post init message

--- db_init/init-db.sh
+++ db_init/init-db.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+
 echo "Fixing permissions for /docker-entrypoint-initdb.d/"
 chown -R mysql:mysql /docker-entrypoint-initdb.d/
 chmod 755 /docker-entrypoint-initdb.d/init-db.sh
 chmod 644 /docker-entrypoint-initdb.d/init.sql
 
 echo "Waiting for MariaDB to be ready..."
-until mariadb -uroot -proot -e "SELECT 1"; do
+while ! mariadb -uroot -proot -h database -e "SELECT 1" &>/dev/null; do
+  echo "MariaDB is not ready yet, retrying in 2s..."
   sleep 2
 done
 
-echo "Running init.sql..."
-mariadb -uroot -proot crm_db < /docker-entrypoint-initdb.d/init.sql
+echo "MariaDB is up! Running init.sql..."
+mariadb -uroot -proot -h database crm_db < /docker-entrypoint-initdb.d/init.sql
+echo "Database initialized successfully!"
